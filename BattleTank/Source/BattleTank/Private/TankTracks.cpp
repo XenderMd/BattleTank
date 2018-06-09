cd61fc6bad36e81c09e1c7f689c94e37d5109ba0
XenderMd <denis.stavilamd@gmail.com>
2018-06-09 19:35:22 +0200

    - Added and applied Throttle force to the tracks - the tank can be now moved - TankTracks:
     auto ForceApplied = GetForwardVector()*Throttle*TrackMaxDrivingForce;
     auto ForceLocation = GetComponentLocation();
     auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
     TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);

--- BattleTank/Source/BattleTank/Private/TankTracks.cpp
+++ BattleTank/Source/BattleTank/Private/TankTracks.cpp
@@ -7,6 +7,13 @@ void UTankTracks::SetThrottle(float Throttle)
 	//auto Time = GetWorld()->GetTimeSeconds();
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s: throttle is  %f"), *Name, Throttle)
+
+	//TODO - clamp the throttle values so that the player cannot overdrive
+	auto ForceApplied = GetForwardVector()*Throttle*TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+
 }
 
 
--- BattleTank/Source/BattleTank/Public/TankTracks.h
+++ BattleTank/Source/BattleTank/Public/TankTracks.h
@@ -22,4 +22,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category=Input)
 	void SetThrottle(float Throttle);
 	
+	//Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000.0; // Assume 40t tank and 1g acceleration
 };
