c4a16e1825ba2c2c010aad5d65ca102d306307c4
XenderMd <denis.stavilamd@gmail.com>
2018-06-09 18:34:45 +0200

    - Added TankTrack components, blueprintspawnable. - Replaced the previous static meshes (simple tracks meshes) with the new components

---
+++ BattleTank/Source/BattleTank/Public/TankTracks.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTracks.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+class BATTLETANK_API UTankTracks : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
